75a4c5ffb5c4496adf274ad8035eba70060f84f2
Muhammad Ashhad <162056028+Rukhsanaashhad@users.noreply.github.com>
2025-04-26 10:26:16 +0500

    app.py

--- app.py
+++ app.py
@@ -1,33 +1,27 @@
 import streamlit as st  
-from PIL import Image, ImageOps, ImageDraw  
+from PIL import Image, ImageOps  
 import io  
 from rembg import remove  
 
-st.set_page_config(page_title="Photo App",
-                   page_icon="🖼️")
-
+st.set_page_config(page_title="Photo App", page_icon="🖼️")
 st.title("Ashhad's Photo Tool 📸")  
 
 uploaded_image = st.file_uploader("Upload an Image", type=["jpg", "jpeg", "png"])  
 
 if 'img_to_display' not in st.session_state:  
     st.session_state.img_to_display = None  
- 
+
 if uploaded_image is not None:  
     img = Image.open(uploaded_image).convert("RGBA")  
     st.image(img, caption="Original Image", use_container_width=True)  
 
     if st.button("Apply Blue Background"):  
-       
         img_no_bg = remove(uploaded_image.getvalue())  
         img_no_bg = Image.open(io.BytesIO(img_no_bg)).convert("RGBA")  
 
-       
         blue_bg = Image.new("RGBA", img_no_bg.size, (0, 102, 204, 255))  
-
         img_alpha = img_no_bg.split()[-1]  
 
-        
         combined_image = Image.new("RGBA", img_no_bg.size)  
         combined_image.paste(blue_bg, (0, 0))  
         combined_image.paste(img_no_bg, (0, 0), img_alpha)  
@@ -40,7 +34,7 @@ if uploaded_image is not None:
         if rotate_angle > 0:  
             rotated_image = st.session_state.img_to_display.rotate(rotate_angle, expand=True)  
             st.session_state.img_to_display = rotated_image  
-            st.image(rotated_image, caption=f"Image rotated by {rotate_angle} degrees", use_container_width=True)  
+            st.image(rotated_image, caption=f"Rotated by {rotate_angle}°", use_container_width=True)  
 
         if st.button("Crop Image"):  
             cropped_img = ImageOps.fit(st.session_state.img_to_display, (300, 300))  
@@ -52,15 +46,7 @@ if uploaded_image is not None:
             st.session_state.img_to_display.save(buf, format="PNG")  
             byte_im = buf.getvalue()  
             st.download_button(label="Download Image", data=byte_im, file_name="blue_background_image.png", mime="image/png")  
-
 else:  
-    st.write("Please upload an Image")  
+    st.write("Please upload an Image.")  
 
-st.write("©️ Developed by [Muhammad Ashhad Khan](https://github.com/Rukhsanaashhad)")
-
-try:
-    with open("requirement.txt", "r") as a:
-        content = a.read()
-        st.text(content)
-except FileNotFoundError:
-    st.warning("requirement.txt file not found.")
+st.markdown("©️ Developed by [Muhammad Ashhad Khan](https://github.com/Rukhsanaashhad)")
